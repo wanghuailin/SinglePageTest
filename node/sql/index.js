be72b5c3d5912c6b10ee6c65a8824aa8cc1febc0
王怀林 <wanghuailin@mdata.cn>
2019-06-12 16:54:35 +0800

    数据库连接

--- node/sql/index.js
+++ node/sql/index.js
@@ -3,13 +3,19 @@
  */
 
 $(function () {
+    var vm_date = new Vue({
+        el: '#vm_date',
+        data: {
+            list: []
+        }
+    })
+
     $.ajax({
         type: "get",
         url: "http://localhost:8081/app/select",
-        data: {},
-        success: function (data) {
-            console.log("----index------");
-            console.log(data);
+        data: "",
+        success: function (date) {
+            vm_date.list = date.data;
         }
     })
 })
--- node/sql/indexServer.js
+++ node/sql/indexServer.js
@@ -12,9 +12,24 @@ var bodyParser = require('body-parser');
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({extended: false})
 
+//可以跨域
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    //这段仅仅为了方便返回json而已
+    res.header("Content-Type", "application/json;charset=utf-8");
+    if (req.method == 'OPTIONS') {
+        //让options请求快速返回
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.get("/app/select", function (req, res) {
     dateCRUD.Select(function (date) {
-        console.log(date);
         res.json({
             data: date
         })
