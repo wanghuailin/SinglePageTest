1111e131c44806b748732efe63f4106dee1cfd0d
王怀林 <wanghuailin@mdata.cn>
2019-06-12 11:30:48 +0800

    尝试node链接sql进行CRUD

--- node/sql/connect.js
+++ node/sql/connect.js
@@ -13,8 +13,33 @@ const config = {
 }
 
 sql.connect(config).then(function () {
+    //查
     new sql.Request().query('select * from test').then(function (recordset) {
         console.log(recordset);
+    }).catch(function (err) {
+        console.log(err);
+    });
+
+    //增
+    var addSql = "INSERT INTO test(num,name,count) VALUES(4,'李',4)";
+    new sql.Request().query(addSql).then(function (result) {
+        console.log('INSERT ID:', result);
+    }).catch(function (err) {
+        console.log(err);
+    });
+
+    //改
+    var updateSql = "UPDATE test SET name= '小' WHERE num=4";
+    new sql.Request().query(updateSql).then(function (result) {
+        console.log('UPDATE affectedRows', result);
+    }).catch(function (err) {
+        console.log(err);
+    });
+
+    //删
+    var updateSql = "DELETE FROM test WHERE num=4";
+    new sql.Request().query(updateSql).then(function (result) {
+        console.log('DELETE affectedRows',result);
     }).catch(function (err) {
         console.log(err);
     });
