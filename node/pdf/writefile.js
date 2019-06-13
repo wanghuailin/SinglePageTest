8180c1d87e9d038bca539c98cc0d339cdac5db3a
王怀林 <wanghuailin@mdata.cn>
2019-06-13 17:28:38 +0800

    pdf可以生成

--- node/pdf/writefile.js
+++ node/pdf/writefile.js
@@ -2,6 +2,7 @@
  * Created by Wang on 2016/12/8.
  */
 
+//测试页面：
 //https://tmshexcube.hexfuture.net/pages/index3.html
 var phantom = require('phantom');//需要引入phantom模块
 
@@ -13,7 +14,6 @@ phantom.create().then(function (ph) {
             } else {
                 //进行pdf生成,加页面返回的status判断,确保页面正常打开.
                 // 给一个定时让要被捕捉的页面的html和js加载完成,否则可能页面会没有加载完就执行了生成pdf操作内容确实
-                console.log(status);
                 setTimeout(function () {
                     page.property('viewportSize', {width: 700, height: 1010});
                     page.render(__dirname + '/output.pdf').then(function () {
