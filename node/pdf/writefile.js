7a68c343ec2f7b419dfcd0ae56fa78709fab858c
王怀林 <wanghuailin@mdata.cn>
2019-06-14 11:23:00 +0800

    提交以下

--- node/pdf/writefile.js
+++ node/pdf/writefile.js
@@ -8,8 +8,11 @@ var phantom = require('phantom');//需要引入phantom模块
 
 phantom.create().then(function (ph) {
     ph.createPage().then(function (page) {
-        page.property('viewportSize', {width: 712, height: 1010});
-        page.open("https://tmshexcube.hexfuture.net/pages/index3.html").then(function (status) { //status有success和fail
+        //viewportSize 是视区的大小，你可以理解为你打开了一个浏览器，然后把浏览器窗口拖到了多大。
+        //clipRect 是裁切矩形的大小，需要四个参数，前两个是基准点，后两个参数是宽高。
+        // page.property('viewportSize', {width: 712, height: 1010});
+        // page.property('clipRect', {top: 0, left: 0, width: 712, height: 1010});
+        page.open("http://lmsbeike.hexfuture.net//Pages/index3.html").then(function (status) { //status有success和fail
             if (status === 'fail') {
                 console.log('open page fail!');
             } else {
@@ -17,6 +20,11 @@ phantom.create().then(function (ph) {
                 // 给一个定时让要被捕捉的页面的html和js加载完成,否则可能页面会没有加载完就执行了生成pdf操作内容确实
                 setTimeout(function () {
                     page.render(__dirname + '/example.png');
+                    // page.property('paperSize', {
+                    //     format: 'A4',
+                    //     orientation: 'portrait',
+                    //     border: '1cm'
+                    // });
                     page.render(__dirname + '/output.pdf').then(function () {
                         console.log('Page rendered');
                         // page.close();
