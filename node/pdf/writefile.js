4480a149fc81d25e3d4c834d80cbb09c98a715bc
王怀林 <wanghuailin@mdata.cn>
2019-06-14 11:54:23 +0800

    提交

--- node/pdf/writefile.js
+++ node/pdf/writefile.js
@@ -12,6 +12,11 @@ phantom.create().then(function (ph) {
         //clipRect 是裁切矩形的大小，需要四个参数，前两个是基准点，后两个参数是宽高。
         // page.property('viewportSize', {width: 712, height: 1010});
         // page.property('clipRect', {top: 0, left: 0, width: 712, height: 1010});
+        page.property('paperSize', {
+            format: 'A4',
+            // orientation: 'portrait',
+            // border: '1cm'
+        });
         page.open("http://lmsbeike.hexfuture.net//Pages/index3.html").then(function (status) { //status有success和fail
             if (status === 'fail') {
                 console.log('open page fail!');
@@ -19,12 +24,7 @@ phantom.create().then(function (ph) {
                 //进行pdf生成,加页面返回的status判断,确保页面正常打开.
                 // 给一个定时让要被捕捉的页面的html和js加载完成,否则可能页面会没有加载完就执行了生成pdf操作内容确实
                 setTimeout(function () {
-                    page.render(__dirname + '/example.png');
-                    // page.property('paperSize', {
-                    //     format: 'A4',
-                    //     orientation: 'portrait',
-                    //     border: '1cm'
-                    // });
+                    page.render(__dirname + '/example.png')
                     page.render(__dirname + '/output.pdf').then(function () {
                         console.log('Page rendered');
                         // page.close();
